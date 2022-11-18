3a7903381d72672c3785b41086fdb4a22244ff1e
Iaroslav Sorokin <iaroslav.sorokin@gmail.com>
2022-11-18 16:26:44 +0300

    Add: moved qcproto basic types here

---
+++ src/types.rs
@@ -0,0 +1,54 @@
+use std::fmt::Display;
+use serde::{Deserialize, Serialize};
+
+const PROTOCOL_VERSION: u16 = 100;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct ActorInfos {
+    pub server: String,
+    pub sender: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum CommandKind {
+    ForwardMessage {
+        from: ActorInfos,
+        to: ActorInfos,
+        content: String,
+    },
+    GetOnlineUsers,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum BotFamily {
+    Discord,
+    Telegram,
+    WhatsApp,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Command {
+    pub kind: CommandKind,
+    pub sender_bot_family: BotFamily,
+    pub protocol_version: u16,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum TransmissionResult {
+    Received,
+    BadSyntax,
+    MismatchedVersions,
+}
+
+impl Display for TransmissionResult {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self {
+            TransmissionResult::Received => write!(f, "Received")?,
+            TransmissionResult::BadSyntax => write!(f, "Bad syntax")?,
+            TransmissionResult::MismatchedVersions => write!(f, "Mismatched protocol versions")?,
+        }
+        Ok(())
+    }
+}
+
+pub struct CommandContext;
